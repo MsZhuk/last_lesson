a97399401f8eb95181962ebf2b13093d25b0405c
Olya <ozhuk.fw@gmail.com>
2024-12-22 18:29:37 +0300

    Update builder.js

--- src/builder.js
+++ src/builder.js
@@ -1,1 +1,30 @@
+import { fa, faker } from '@faker-js/faker';
+
+export class UserBuilder {
+  addComment(){
+    this.testComment = faker.lorem.text();
+    return this;
+  };
+  addAuthor(){
+    this.testAuhor = faker.person.firstName();
+    return this;
+  };
+
+  addEmail(){
+    this.testEmail = faker.internet.email();
+    return this;
+  };
+
+  generate() {
+    const copied = structuredClone (
+      {
+      testComment : this.testComment,
+      testAuhor : this.testAuhor,
+      testEmail : this.testEmail
+    }
+    )
+    return copied;
+  }
+
+}
 
